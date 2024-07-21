54a82e1243ee6f5a15f1d6c12b65257569d64938
ccheng <76075charlie@gmail.com>
2024-07-21 22:45:02 +0800

    oblique

--- IRcar.ino
+++ IRcar.ino
@@ -1,56 +1,83 @@
 #include <Servo.h>
-
+#include <Wire.h>
+#include <MPU6050.h>
 
 Servo servoL;
 Servo servoR;
+MPU6050 mpu;
 
 // 定義腳位
-#define IR1 2
-#define IR2 3
-#define IR3 4
-#define IR4 7
-
+#define S1 2
+#define S2 3
+#define S3 4
+#define S4 5
+#define S5 8
 // 定義馬達
 #define motorL 9
 #define motorR 10
 
 // 初始化函式
 void setup() {
+  Serial.begin(9600);
   servoL.attach(motorL);
   servoR.attach(motorR);
+  Wire.begin();
+  mpu.initialize();
   
-  pinMode(IR1, INPUT);
-  pinMode(IR2, INPUT);
-  pinMode(IR3, INPUT);
-  pinMode(IR4, INPUT);
-
-
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  pinMode(S5, INPUT);
 }
-
-//[Return 1 when WHITE  |  Return 0 when BLACK]
 
 void loop() {
-  int sen1 = digitalRead(IR1);
-  int sen2 = digitalRead(IR2);//L
-
-  int sen3 = digitalRead(IR3);
-  int sen4 = digitalRead(IR4);//R
- 
+  int sen1 = digitalRead(S1);
+  int sen2 = digitalRead(S2);
+  int sen3 = digitalRead(S3);
+  int sen4 = digitalRead(S4);
+  int16_t ax, ay, az;
   
-  if(sen2 == 0 || sen3 == 0){ // 直行
-    servoL.writeMicroseconds(1800);
-    servoR.writeMicroseconds(1800);
+  mpu.getAcceleration(&ax, &ay, &az);
+  float oblique = atan2(ay, az) * 180 / PI;
+  
+  Serial.print("Roll = "); 
+  Serial.println(roll);
+  Serial.print("S4: ");
+  Serial.println(sen4);
+
+  delay(1000);
+}
+  
+  if(sen1 == 1 && sen2 == 1 ) { // 直行
+    if(oblique >= 10){
+    servoL.writeMicroseconds(1500);
+    servoR.writeMicroseconds(1582);       
   }
-  else if(sen3 == 1 && sen2 == 0){ //車子偏左(左馬達加速)
-    servoL.writeMicroseconds(1850);
+  else{
+    servoL.writeMicroseconds(1600);
+    servoR.writeMicroseconds(1682);    
+    }
+
+  }
+  else if(sen1 == 0 && sen2 == 1 ) { // 車子偏左
+    servoL.writeMicroseconds(1500);
     servoR.writeMicroseconds(1750);
   }
-  else if(sen3 == 0 && sen2 == 1){ //車子偏右(右馬達加速)
+  else if(sen1 == 1 && sen2 == 0) { // 車子偏右
     servoL.writeMicroseconds(1750);
-    servoR.writeMicroseconds(1850);
+    servoR.writeMicroseconds(1500);
   }
-delay(10);
-}
+  else if(sen1 == 0 && sen2 == 0){
+    if (sen4 == 1){
+    servoL.writeMicroseconds(1600);
+    servoR.writeMicroseconds(1682);      
+  }
+   else{
+    servoL.writeMicroseconds(1500);
+    servoR.writeMicroseconds(1500);    
+    }
+    }  
 
 
 
