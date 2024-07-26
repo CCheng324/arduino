a646d5c4347a67b7e06fbbb680bf1dce10a40887
CCheng324 <140939917+CCheng324@users.noreply.github.com>
2024-07-27 02:17:57 +0800

    final

--- IRcar.ino
+++ IRcar.ino
@@ -12,6 +12,8 @@ MPU6050 mpu;
 #define S3 4
 #define S4 5
 #define S5 6 
+#define det_R 11
+#define det_L 12
 // 定義馬達
 #define motorL 9
 #define motorR 10
@@ -35,15 +37,19 @@ void setup() {
   pinMode(S3, INPUT);
   pinMode(S4, INPUT);
   pinMode(S5, INPUT);
+  pinMode(det_R, INPUT);
+  pinMode(det_L, INPUT); 
 }
 
 // 獲取感測器數據
-void getSensorData(int &sen1, int &sen2, int &sen3, int &sen4, int &sen5) {
+void getSensorData(int &sen1, int &sen2, int &sen3, int &sen4, int &sen5, int &detR, int &detL) {
   sen1 = digitalRead(S1);
   sen2 = digitalRead(S2);
   sen3 = digitalRead(S3);
   sen4 = digitalRead(S4);
   sen5 = digitalRead(S5);
+  detR = digitalRead(det_R);
+  detL = digitalRead(det_L);  
 }
 
 // 獲取傾斜角度
@@ -68,49 +74,46 @@ void detectOblique(float obl) {
 }
 
 // 控制車子轉向
-void controlCar(int sen1, int sen2, int sen3, int sen4, int sen5) {
-  if (sen1 == 1 && sen2 == 1 && sen3 == 1 && sen4 == 1 && sen5 == 1) { // 全黑停車
+void controlCar(int sen1, int sen2, int sen3, int sen4, int sen5, int detR, int detL) {
+  if (sen1 == 1 && sen2 == 1 && sen3 == 1 && sen4 == 1 && sen5 == 1) { // 全白停車
     servoL.writeMicroseconds(stop_speed);
     servoR.writeMicroseconds(stop_speed);
-  } else if (sen1 == 1 && sen2 == 1 && sen3 == 1 && sen4 == 1) {
+  } else if (detR == 1 && sen1 == 0 && sen2 == 0 && sen3 == 0 && sen4 == 0 && sen5 == 0 && detL == 0) {
+    servoL.writeMicroseconds(stop_speed);
+    servoR.writeMicroseconds(Vshape_turn);        
+  } else if (detL == 1 && sen1 == 0 && sen2 == 0 && sen3 == 0 && sen4 == 0 && sen5 == 0 && detR == 0) {
     servoL.writeMicroseconds(Vshape_turn);
-    servoR.writeMicroseconds(stop_speed);
-  } else if (sen2 == 1 && sen3 == 1 && sen4 == 1 && sen5 == 1 ) { 
-    servoL.writeMicroseconds(stop_speed);
-    servoR.writeMicroseconds(Vshape_turn);    
-
-
-
-
-
-
-    
-  } else if (sen3 == 1 && (sen1 == 1 || sen2 == 1)) {// T(右彎)
+    servoR.writeMicroseconds(stop_speed); 
+  } else if (sen3 == 1 && sen1 == 1 && sen2 == 0 && sen4 == 0 && sen5 == 0) { // V-shape(右彎)
+    delay(200);
+    servoL.writeMicroseconds(Vshape_turn);
+    servoR.writeMicroseconds(1350);
+    delay(550);
+  } else if (sen3 == 1 && sen5 == 1 && sen1 == 0 && sen2 == 0 && sen4 == 0) { // V-shape(左彎)
+    delay(200);
+    servoL.writeMicroseconds(1350);
+    servoR.writeMicroseconds(Vshape_turn);
+    delay(550);
+  } else if (sen3 == 1 && sen1 == 1 && sen2 == 1) { // T(右彎)
     servoL.writeMicroseconds(stop_speed);
     servoR.writeMicroseconds(stop_speed);
     delay(100);
     servoL.writeMicroseconds(Vshape_turn);
     servoR.writeMicroseconds(stop_speed);
     delay(500);
-  } else if (sen3 == 1 && (sen4 == 1 || sen5 == 1)) { //T(左彎)
+  } else if (sen3 == 1 && sen4 == 1 && sen5 == 1) { // T(左彎)
     servoL.writeMicroseconds(stop_speed);
     servoR.writeMicroseconds(stop_speed);
     delay(100);    
     servoL.writeMicroseconds(stop_speed);
     servoR.writeMicroseconds(Vshape_turn);
     delay(500);
-  } else if ((sen2 == 0 && sen4 == 1) || sen5 == 1) { // 車子偏右 
+  } else if ((sen2 == 0 && sen4 == 1 && sen3 == 0) || (sen1 == 0 && sen2 == 0 && sen3 == 0 && sen4 == 0 && sen5 == 1)) { // 車子偏右 
     servoL.writeMicroseconds(stop_speed);
     servoR.writeMicroseconds(turn);
-  } else if ((sen2 == 1 && sen4 == 0) || sen1 == 1) { // 車子偏左 
+  } else if ((sen2 == 1 && sen4 == 0 && sen3 == 0) || (sen1 == 1 && sen2 == 0 && sen3 == 0 && sen4 == 0 && sen5 == 0)) { // 車子偏左 
     servoL.writeMicroseconds(turn);
     servoR.writeMicroseconds(stop_speed);
-  } else if (sen1 == 1) { // 車子偏左 
-    servoL.writeMicroseconds(turn);
-    servoR.writeMicroseconds(stop_speed);
-  } else if (sen5 == 1) { // 車子偏右 
-    servoL.writeMicroseconds(stop_speed);
-    servoR.writeMicroseconds(turn);
   } else { // 直行
     servoL.writeMicroseconds(basic_motor_L);
     servoR.writeMicroseconds(basic_motor_R);
@@ -118,11 +121,11 @@ void controlCar(int sen1, int sen2, int sen3, int sen4, int sen5) {
 }
 
 void loop() {
-  int sen1, sen2, sen3, sen4, sen5;
-  getSensorData(sen1, sen2, sen3, sen4, sen5);
+  int sen1, sen2, sen3, sen4, sen5, detR, detL;
+  getSensorData(sen1, sen2, sen3, sen4, sen5, detR, detL);
 
   float oblique = getObliqueAngle();
   detectOblique(oblique);
 
-  controlCar(sen1, sen2, sen3, sen4, sen5);
+  controlCar(sen1, sen2, sen3, sen4, sen5, detR, detL);
 }
